a8b07da34b724851cbdbe6c3ba90fabfe0d2a10f
ak7589 <asinha.kumar@gmail.com>
2019-10-12 13:06:51 +0530

    Initial Commit

---
+++ ride_tracker-master_spring_jdbc/src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -0,0 +1,47 @@
+package com.pluralsight.controller;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.pluralsight.model.Ride;
+
+import org.junit.Test;
+
+public class RestControllerTest {
+
+	@Test(timeout=3000)
+	public void testCreateRide() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//Create a Ride Object
+		Ride ride = new Ride();
+		
+		ride.setName("Ride to Johannesberg");
+		ride.setDuration(35);
+		
+		//Make a rest call
+		restTemplate.put("http://localhost:8080/ride_tracker_spring_jdbc/ride", ride);
+		
+	}
+	
+	
+	@Test(timeout=3000)
+	public void testGetRides() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
+				"http://localhost:8080/ride_tracker_spring_jdbc/rides", HttpMethod.GET,
+				null, new ParameterizedTypeReference<List<Ride>>() {
+				});
+		List<Ride> rides = ridesResponse.getBody();
+
+		for (Ride ride : rides) {
+			System.out.println("Ride name: " + ride.getName());
+		}
+	}
+}
